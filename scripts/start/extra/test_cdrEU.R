9ebcd79b72f6b92fa792271cc114ff5a5cc70222
Kristine Karstens <karstens@pik-potsdam.de>
2025-09-10 22:08:28 +0200

    new revision

--- scripts/start/extra/test_cdrEU.R
+++ scripts/start/extra/test_cdrEU.R
@@ -12,7 +12,7 @@
 library(magpie4)
 library(magclass)
 
-version <- "EUCDR-04"
+version <- "EUCDR-05"
 
 # Load start_run(cfg) function which is needed to start MAgPIE runs
 source("scripts/start_functions.R")
